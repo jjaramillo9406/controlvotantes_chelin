4182a23090d96c312ddb4e2c8e4ef7f97c0d7c99
Jorge Alexander Jaramillo Ortega <118781768+jjaramillo9406@users.noreply.github.com>
2026-01-29 17:47:18 -0500

    Merge

--- apps/core/routines/masivo_votantes_routine.py
+++ apps/core/routines/masivo_votantes_routine.py
@@ -35,12 +35,12 @@ def load_votantes(data):
                         else:
                             errors.append(f"{index}: Municipio no validos")
 
-                        if row[7] != "" and not municipio is None:
-                            puesto = Puesto.objects.filter(nombre__icontains=row[7]).first()
-                            if puesto is None:
-                                errors.append(f"{index}: Puesto {row[7]} no existe")
-                        else:
-                            errors.append(f"{index}: Puesto no validos")
+                        # if row[7] != "" and not municipio is None:
+                        #     puesto = Puesto.objects.filter(nombre__icontains=row[7]).first()
+                        #     if puesto is None:
+                        #         errors.append(f"{index}: Puesto {row[7]} no existe")
+                        # else:
+                        #     errors.append(f"{index}: Puesto no validos")
 
 
                         if row[9] != "":
@@ -59,11 +59,11 @@ def load_votantes(data):
                                 votante.telefono = row[3]
                                 votante.direccion = row[4]
                                 votante.municipio_id = municipio.id
-                                votante.puesto_id = puesto.id
-                                if row[8] != "" and str(row[8]) != "nan":
-                                    votante.mesa = row[8]
-                                else:
-                                    votante.mesa = 0
+                                #votante.puesto_id = puesto.id
+                                #if row[8] != "" and str(row[8]) != "nan":
+                                #    votante.mesa = row[8]
+                                #else:
+                                #    votante.mesa = 0
                                 votante.usuario_id = user.id
                                 votante.asistio = False
                                 votante.referido = "N/A"
