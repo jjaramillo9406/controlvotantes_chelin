082959aec0ee73a766a6a9b018deab92929eb1ca
Jorge Alexander Jaramillo Ortega <118781768+jjaramillo9406@users.noreply.github.com>
2026-02-13 15:31:51 -0500

    Merge

--- apps/core/routines/masivo_votantes_routine.py
+++ apps/core/routines/masivo_votantes_routine.py
@@ -9,66 +9,69 @@ def load_votantes(data):
     errors = []
     has_errors = False
     df = pd.read_excel(data)
-    with transaction.atomic():
-        for index, row in df.iterrows():
-            municipio = None
-            puesto = None
-            user = None
-            if len(errors) > 0:
-                raise Exception(errors[0])
-            else:
-                if index >= 0:
-                    row[2] = str(row[2]).replace(" ", "")
-                    if row[0] == "":
-                        errors.append(f"{index}: Nombres del votante no validos")
+    try:
+        with transaction.atomic():
+            for index, row in df.iterrows():
+                municipio = None
+                puesto = None
+                user = None
+                if len(errors) > 0:
+                    raise Exception(errors[0])
+                else:
+                    if index >= 0:
+                        row[2] = str(row[2]).replace(" ", "")
+                        if row[0] == "":
+                            errors.append(f"{index}: Nombres del votante no validos")
 
-                    if row[1] == "":
-                        errors.append(f"{index}: Apelidos del votante no validos")
+                        if row[1] == "":
+                            errors.append(f"{index}: Apelidos del votante no validos")
 
-                    if row[2] == "":
-                        errors.append(f"{index}: Documento del votante no valido")
+                        if row[2] == "":
+                            errors.append(f"{index}: Documento del votante no valido")
 
-                    if row[5] != "":
-                        municipio = Municipio.objects.filter(nombre__icontains=row[5]).first()
-                        if municipio is None:
-                            errors.append(f"{index}: Municipio {row[5]} no existe")
-                    else:
-                        errors.append(f"{index}: Municipio no validos")
+                        if row[5] != "":
+                            municipio = Municipio.objects.filter(nombre__icontains=row[5]).first()
+                            if municipio is None:
+                                errors.append(f"{index}: Municipio {row[5]} no existe")
+                        else:
+                            errors.append(f"{index}: Municipio no validos")
 
-                    if row[7] != "" and not municipio is None:
-                        puesto = Puesto.objects.filter(nombre__icontains=row[7]).first()
-                        if puesto is None:
-                            errors.append(f"{index}: Puesto {row[7]} no existe")
-                    #else:
-                    #     errors.append(f"{index}: Puesto no validos")
+                        if row[7] != "" and not municipio is None:
+                            puesto = Puesto.objects.filter(nombre__icontains=row[7]).first()
+                            if puesto is None:
+                                errors.append(f"{index}: Puesto {row[7]} no existe")
+                        # else:
+                        #     errors.append(f"{index}: Puesto no validos")
 
-                    if row[9] != "":
-                        user = User.objects.filter(email__iexact=row[9].strip()).first()
-                        if user is None:
-                            errors.append(f"{index}: Usuario no existe")
-                    else:
-                        errors.append(f"{index}: Usuario no valido")
+                        if row[9] != "":
+                            user = User.objects.filter(email__iexact=row[9].strip()).first()
+                            if user is None:
+                                errors.append(f"{index}: Usuario no existe")
+                        else:
+                            errors.append(f"{index}: Usuario no valido")
 
-                    if len(errors) == 0:
-                        if not Votante.objects.filter(identificacion=row[2]).exists():
-                            votante = Votante()
-                            votante.nombres = row[0].upper()
-                            votante.apellidos = row[1].upper()
-                            votante.identificacion = row[2]
-                            votante.telefono = row[3]
-                            votante.direccion = row[4]
-                            votante.municipio_id = municipio.id
-                            votante.puesto_id = None
-                            votante.puesto = None
-                            votante.mesa = 0
-                            # votante.puesto_id = puesto.id
-                            # if row[8] != "" and str(row[8]) != "nan":
-                            #    votante.mesa = row[8]
-                            # else:
-                            #    votante.mesa = 0
-                            votante.usuario_id = user.id
-                            votante.asistio = False
-                            votante.referido = "N/A"
-                            votante.full_clean()
-                            votante.save()
+                        if len(errors) == 0:
+                            if not Votante.objects.filter(identificacion=row[2]).exists():
+                                votante = Votante()
+                                votante.nombres = row[0].upper()
+                                votante.apellidos = row[1].upper()
+                                votante.identificacion = row[2]
+                                votante.telefono = row[3]
+                                votante.direccion = row[4]
+                                votante.municipio_id = municipio.id
+                                votante.puesto_id = None
+                                votante.puesto = None
+                                votante.mesa = 0
+                                # votante.puesto_id = puesto.id
+                                # if row[8] != "" and str(row[8]) != "nan":
+                                #    votante.mesa = row[8]
+                                # else:
+                                #    votante.mesa = 0
+                                votante.usuario_id = user.id
+                                votante.asistio = False
+                                votante.referido = "N/A"
+                                votante.full_clean()
+                                votante.save()
+    except:
+        return errors
     return errors
