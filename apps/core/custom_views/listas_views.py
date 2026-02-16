019e6d4ac89d2d5d8966669318093fcf684c59e2
Jorge Alexander Jaramillo Ortega <118781768+jjaramillo9406@users.noreply.github.com>
2026-02-16 10:46:28 -0500

    Se agrega filtro para obtener listado de capturadores cuando el usuario consultado en la lista de votantes sea orientador

--- apps/core/custom_views/listas_views.py
+++ apps/core/custom_views/listas_views.py
@@ -15,10 +15,20 @@ def index(request):
     votantes = []
     if request.method == "POST":
         if request.POST.get('user_id') != 0:
-            if request.user.user_config.nivel == 99:
-                votantes = Votante.objects.filter(usuario_id=request.POST.get('user_id'))[:1000]
-            if request.user.user_config.nivel == 90:
-                votantes = Votante.objects.filter(usuario_id=request.POST.get('user_id'), usuario__user_config__orientador_id=request.user.id)[:1000]
+            usuario = User.objects.filter(id=request.POST.get('user_id')).first()
+            if usuario.user_config.nivel == 90:
+                capturadores = User.objects.filter(user_config__orientador_id=usuario.id)
+                capturadores_id = []
+                for capturador in capturadores:
+                    capturadores_id.append(capturador.id)
+
+                votantes = Votante.objects.filter(usuario_id__in=capturadores_id)
+            else:
+                if request.user.user_config.nivel == 99:
+                    votantes = Votante.objects.filter(usuario_id=request.POST.get('user_id'))[:1000]
+                if request.user.user_config.nivel == 90:
+                    votantes = Votante.objects.filter(usuario_id=request.POST.get('user_id'),
+                                                      usuario__user_config__orientador_id=request.user.id)[:1000]
             selected = request.POST.get('user_id')
         else:
             if request.user.user_config.nivel == 99:
